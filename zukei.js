c2216ae4e2ce46bd84813a89e5fe4233abdffc54
福井章吾 <fukuishougo@net43-dhcp119.sfc.keio.ac.jp>
2018-12-11 11:17:34 +0900

    add

---
+++ zukei.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+ctx.lineWidth = 10.0;
+ctx.strokeStyle = 'black';
+ctx.fillStyle = 'lightgrey';
+
+ctx.fillRect(50, 50, 100, 100);
+ctx.strokeRect(50, 50, 100, 100);
